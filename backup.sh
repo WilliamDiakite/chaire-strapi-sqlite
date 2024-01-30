fc8b648dadd1ae73fce35129e5c9b34d4dc4c03a
william.diakite@gmail.com <william.diakite@gmail.com>
2024-01-30 14:51:23 -0500

    category and axis are required for archives

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 filename="./backups/strapi-$today.db"
 echo "Backing up strapi.db to $filename"
-scp -s srv-cio541tgkuvudi8r97s0@ssh.oregon.render.com:/data/strapi.db $filename
+scp -s srv-cjal7nue546c73avn3s0@ssh.oregon.render.com:/data/strapi.db $filename
